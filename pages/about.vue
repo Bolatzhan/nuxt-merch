4a05485387d4906ce59c0018f4c771a3e2cf9ba4
Bula <klara23.03.1900@gmail.com>
2024-05-23 02:51:05 +0500

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<h2>About</h2>
+		<p>
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet optio odio
+			repudiandae itaque rerum deserunt neque assumenda dolore impedit natus.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur nam
+			at architecto sit quo in repudiandae ducimus ipsa aliquid facilis!
+		</p>
+	</div>
+	<div>{{ data }}</div>
+</template>
+
+<script setup lang="ts">
+	const { data } = await useFetch('/api/hello?name=mario', {
+		method: 'post',
+		body: { age: 30 },
+	});
+</script>
+
+<style scoped>
+	h2 {
+		margin-bottom: 20px;
+		font-size: 36px;
+	}
+
+	p {
+		margin: 20px 0px;
+	}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<h2>Home</h2>
+		<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet optio odio repudiandae itaque rerum deserunt
+			neque assumenda dolore impedit natus.</p>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur nam at architecto sit quo in repudiandae
+			ducimus ipsa aliquid facilis!</p>
+	</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+	margin-bottom: 20px;
+	font-size: 36px;
+}
+
+p {
+	margin: 20px 0px;
+}
+</style>
